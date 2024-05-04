b1c9ef531f2a0b9091a3dad89c2eae0e12149e40
2trill2code <s4007180@student.rmit.edu.au>
2024-05-04 20:10:55 +1000

    add guest

---
+++ src/Guest.java
@@ -0,0 +1,9 @@
+public class Guest {
+    private String sessionID;
+    private boolean isBlackListed;
+
+    public void viewPost(String postID) {};
+    public void login() {};
+    public void register() {};
+
+}
