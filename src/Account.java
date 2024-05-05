30c0721c8482fcd33058a1d7fcece693dfd63c18
Falah Rasyidi <s4007133@student.rmit.edu.au>
2024-05-05 12:39:48 +1000

    Added param and return javadoc comments

--- src/Account.java
+++ src/Account.java
@@ -6,10 +6,51 @@ public class Account {
     private String email;
     private String password;
 
-    public void viewPost() { };
+    /**
+     * @param postID The ID of the post to be viewed
+     * @return No return value
+     */
+    public void viewPost(String postID) { return; };
+
+    /**
+     * @param void
+     * @return The ID of the post
+     */
     public void createPost() { };
-    public void answerQuestion() { };
-    public void makeComment() { };
-    public void flagComment() { };
-    public void votePost() { };
+
+    /**
+     * @param void
+     * @return The ID of the question
+     */
+    public void createQuestion() { };
+
+    /**
+     * @param postID The ID of the post (question) being answered
+     * @return The ID of the answer
+     */
+    public void createAnswer(String postID) { };
+
+    /**
+     * @param postID The ID of the post (question/answer/post) being commented on
+     * @return The ID of the comment
+     */
+    public void createComment(String postID) { };
+
+    /**
+     * @param postID The ID of the post being flagged
+     * @return No return value
+     */
+    public void flagPost(String postID) { return; };
+
+    /**
+     * @param commentID The ID of the comment being flagged
+     * @return No return value
+     */
+    public void flagComment(String commentID) { return; };
+
+    /**
+     * @param postID The ID of the post being voted on
+     * @return No return value
+     */
+    public void votePost(String postID) { return; };
 }
